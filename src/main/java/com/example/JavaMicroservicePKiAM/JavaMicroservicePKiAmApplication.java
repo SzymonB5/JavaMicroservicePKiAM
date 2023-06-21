98a83779ed54caf29ca5de37089741675b3248b3
SzymonB5 <235831@edu.p.lodz.pl>
2023-06-21 21:51:45 +0200

    Initial commit

---
+++ src/main/java/com/example/JavaMicroservicePKiAM/JavaMicroservicePKiAmApplication.java
@@ -0,0 +1,13 @@
+package com.example.JavaMicroservicePKiAM;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaMicroservicePKiAmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaMicroservicePKiAmApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/JavaMicroservicePKiAM/JavaMicroservicePKiAmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.JavaMicroservicePKiAM;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaMicroservicePKiAmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
